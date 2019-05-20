be26771a36131db36870a31f2af6f57a4da47797
mullencm <44680350+mullencm@users.noreply.github.com>
2019-05-20 11:09:47 -0400

    Add files via upload

---
+++ auto_runtime.py
@@ -0,0 +1,10 @@
+
+
+
+while True:
+    H = int(input("give me an H: "))
+
+    if (12 > H) or (H > 17) or (H == 17):
+        print("    CODE SHALL NOT RUN")
+    if ((12 < H) and (H < 17)) or (H == 12):
+        print("LETS RUN SOME CODE")
